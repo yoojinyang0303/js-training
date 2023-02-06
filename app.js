aa79dfedc03d873b47ccf9dfa4809a7f0da6e4e9
yjyang <yoojinyang0303@gmail.com>
2023-02-07 00:04:56 +0900

    Fix: 토글 상태 로컬스토리지 값 변경 안 되는 문제 해결

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // 로컬스토리지 데이터 읽기
 let toggleMode = localStorage.getItem("toggleMode");
 const sideButton = document.querySelector("i");
-const navBar = document.querySelector("nav");
+let navBar = document.querySelector("nav");
 
 const activeToggleMode = () => {
   navBar.classList.add("active");
@@ -14,25 +14,24 @@ const disableToggleMode = () => {
   navBar.classList.remove("active");
   localStorage.setItem("toggleMode", null);
 };
-
-// if (toggleMode === "active") {
-//   activeToggleMode();
-// } else {
-//   disableToggleMode();
-// }
 if (toggleMode === "active") {
-  disableToggleMode();
-} else {
   activeToggleMode();
 }
 
 sideButton.addEventListener("click", () => {
-  toggleMode = localStorage.getItem("toggleMode");
-  if (toggleMode !== "active") {
-    activeToggleMode();
-  } else {
-    // navBar.classList.remove("active");
-    // localStorage.setItem("toggleMode", null);
+  // toggleMode = localStorage.getItem("toggleMode");
+  if (toggleMode == "active") {
     disableToggleMode();
+  } else {
+    activeToggleMode();
   }
 });
+
+// sideButton.addEventListener("click", () => {
+//   toggleMode = localStorage.getItem("toggleMode");
+//   if (toggleMode !== "active") {
+//     activeToggleMode();
+//   } else {
+//     disableToggleMode();
+//   }
+// });
