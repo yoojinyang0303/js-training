5247cb0ab76e11259b1e42271fcdd88f71e67b72
yjyang <yoojinyang0303@gmail.com>
2023-02-14 00:11:04 +0900

    Feat: NewsList.js 작성

--- App.js
+++ App.js
@@ -1,1 +1,32 @@
 // do something!
+import Nav from "./components/Nav";
+import NewsList from "./components/NewsList";
+
+window.onload = async function () {
+  const root = document.getElementById("root");
+  const proxyData = new Proxy(
+    {
+      category: "all",
+    },
+    {
+      set: async function (target, prop, value) {
+        Reflect.set(target, prop, value);
+        const newsListElement = await NewsList(proxyData);
+        const newsListContainer = root.querySelector(".news-list-container");
+
+        if (newsListContainer === null) {
+          root.appendChild(newsListElement);
+        } else {
+          newsListContainer.replaceWith(newsListElement);
+          return;
+        }
+      },
+    }
+  );
+
+  const navElement = Nav(proxyData);
+  root.appendChild(navElement);
+
+  const newsListElement = await NewsList(proxyData);
+  root.appendChild(newsListElement);
+};
