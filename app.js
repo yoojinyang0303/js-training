93db27602463f4009217b154eb9dace3e3289fa8
yjyang <yoojinyang0303@gmail.com>
2023-02-14 00:27:14 +0900

    chore: esLint 사용

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-import AnalogClock from './AnalogClock.js';
+import AnalogClock from "./AnalogClock.js";
 
-document.querySelectorAll('.analog-clock').forEach(AnalogClock);
+document.querySelectorAll(".analog-clock").forEach(AnalogClock);
