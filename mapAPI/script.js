709242925c18e3cc91fc4102cd9ccf608e387de8
yjyang <yoojinyang0303@gmail.com>
2023-03-01 16:57:49 +0900

    feat: 카카오 맵 API 키 연결 및 지도 화면 불러오기

---
+++ mapAPI/script.js
@@ -0,0 +1,44 @@
+(function () {
+  "use strict";
+
+  const shops = [
+    {
+      id: 1292273001,
+      name: "매콤돈가스&칡불냉면 판교점",
+      lat: 37.40189834738935,
+      lng: 127.10624455094185,
+    },
+    {
+      id: 1151112822,
+      name: "탄탄면공방 판교테크노밸리점",
+      lat: 37.40193038525563,
+      lng: 127.11060980539878,
+    },
+    {
+      id: 15775065,
+      name: "파리바게뜨 판교테크노점",
+      lat: 37.40133360873933,
+      lng: 127.10801128231743,
+    },
+  ];
+
+  const defaultPos = {
+    lat: 37.4020589,
+    lng: 127.1064401,
+  };
+
+  const get = (target) => {
+    return document.querySelector(target);
+  };
+
+  const $map = get("#map");
+
+  const mapContainer = new kakao.maps.Map($map, {
+    center: new kakao.maps.LatLng(defaultPos.lat, defaultPos.lng),
+    level: 3, // 지도의 레벨(확대, 축소 정도)
+  });
+
+  const init = () => {};
+
+  init();
+})();
